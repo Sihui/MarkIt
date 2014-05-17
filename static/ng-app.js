c4be1be3cb2167323fbe2defcd21198978a2b03d
Sihui Huang <sihuih@gmail.com>
2014-05-17 14:19:45 -0500

    dispalyed directivedForceGraph

--- static/ng-app.js
+++ static/ng-app.js
@@ -44,65 +44,27 @@
             };
         });
     app.directive('forcedirectedGraph',['$filter','$log',function($filter,$log){
-             /*function createLink (s,t,v){
-                  return {source: s,
-                        target: t,
-                         value:v};
-                };*/
-        
-            /*function getLinks(scope,element,filter){
-                    scope.links = [];
-                    angular.forEach(tags, function(tag){
-                        if(tag.Friends!=0){
-                            //convert the json list into javascript array
-                            tag.Friends = tag.Friends.join(',').split(',');
-                            var fList = $filter('orderBy')(tag.Friends, string);
-                            if(fList.length==1){//
-                                scope.links.push(createLink(tags.indexOf(tag),tags.indexOf(fList[0]),1));
-                            }else{  
-                                for(var i=0;i<fList.length-2;i++){
-                                    var value=1;
-                                    while(i+1<(fList.length-1)&&fList[i]==fList[i+1]){
-                                        value++;
-                                        i++;
-                                    }
-                                    //last element
-                                    if(i+1==(fList.length-1)){
-                                        //determine last element
-                                        if(fList[i]==fList[i+1]){
-                                        value++;
-                                        scope.links.push(createLink(tags.indexOf(tag),tags.indexOf(fList[i]),value));    
-                                        }else{
-                                        scope.links.push(createLink(tags.indexOf(tag),tags.indexOf(fList[i]),value));  
-                                        scope.links.push(createLink(tags.indexOf(tag),tags.indexOf(fList[i+1]),1));
-                                        }
-                                    }
-                                    //current element isn't the second last one
-                                    else{
-                                        scope.links.push(createLink(tags.indexOf(tag),tags,indexOf(fList[i]),value));        
-                                    }
-                                }
-                            } //                        
-                        }
-                    });
-                    return angular.toJson(scope.links);
-                }*/
             return {
                 restrict: 'E',
                 scope: { tags: '='},
-                template:'<p ng-style="ForceDirectedGraph()">{{}}</p>',
+                template:'<p>{{init()}}</p>',
                 link: function ( $scope, $element,attr){
-                     
                             function createLink (s,t,v){
-                                
                                                     return {source: s,
                                                             target: t,
                                                              value:v};
                                                 };
-                            
-                            $scope.getLinks = function(tags){
-                                
+                            $scope.init = function(){
+                                $log.log("INIT-DATA");
+                                $log.log($scope.tags);
+                                $log.log("END-INIT-DATA");
+                                $scope.getLinks();
+                                ForceDirectedGraph($scope.tags,$scope.links,$scope.tags);
+                                return 1;
+                            }
+                            $scope.getLinks = function(){
                                         $scope.links = [];
+                                        tags = $scope.tags;
                                         angular.forEach(tags, function(tag){
                                             if(tag.Friends!=0){
                                                 //convert the json list into javascript array
@@ -112,7 +74,7 @@
                                                 //get a list of tag name
                                                 var fNameList = [];
                                                 angular.forEach(tags, function(tag){fNameList.push(tag.Tag)});
-                                                $log.log(tag.Friends);
+                                                //$log.log(tag.Friends);
                                                 if(fList.length==1){
                                                     $scope.links.push(createLink(tags.indexOf(tag),tags.indexOf(fList[0]),1));
                                                 }else{  
@@ -137,7 +99,7 @@
                                                         }
                                                         //current element isn't the second last one
                                                         else{
-                                                       //     $log.log("push 3");
+                                                       //   $log.log("push 3");
                                                             $scope.links.push(createLink(tags.indexOf(tag),fNameList.indexOf(fList[i]),value)); 
                                                             /* $log.log("START");
                                                              $log.log(tags);
@@ -149,51 +111,58 @@
                                                 }                  
                                             }
                                         });
-                                        $scope.JSONLinks = angular.toJson($scope.links); 
-                                        $scope.JSONTags = angular.toJson(tags); 
+                                        //$scope.JSONLinks = angular.toJson($scope.links); 
+                                        //$scope.JSONTags = angular.toJson(tags); 
                                  //$log.log(angular.toJson($scope.links));
-                                         //$log.log("LOOK");
-                                        // $log.log($scope.JSONTags);
-                                return $scope.JSONLinks
-                                        
-                            
+                                         $log.log("END");
+                                        //$log.log($scope.JSONTags);
+                                //return $scope.JSONLinks                                                              
                             }
-                            
-                            //
-                           function ForceDirectedGraph(){
-                                $log.log("LOOK");
+                           function ForceDirectedGraph(tags,JSONLinks,JSONTags){
+                                //$log.log("FDG-DATA");
+                                //$log.log(tags);
+                                //$log.log(JSONLinks);
+                                //$log.log(JSONTags);
+                                //$log.log("END-FDG-DATA");
                                 $scope.getLinks(tags);
+                                $log.log("FDG-1");
                                 var width = 960,
                                     height = 500;
-
-                                var color = d3.scale.category20();
-
+                               $log.log("FDG-2");
                                 var force = d3.layout.force()
                                     .charge(-120)
                                     .linkDistance(30)
-                                    .size([width, height]);
+                                    .size([width, height])
+                                    .nodes(JSONTags)
+                                    .links(JSONLinks)
+                                    .start();
 
-                                var svg = d3.select("body").append("svg")
+                                var svg = d3.select("k").append("svg")
                                     .attr("width", width)
                                     .attr("height", height);
-                                force
-                                      .nodes($scope.JSONTags)
-                                      .links($scope.JSONLinks)
-                                      .start();
+                               $log.log("FDG-3");
+                              /* force.nodes(JSONTags)
+                                      .links(JSONLinks)
+                                        .start();*/
+                               $log.log("FDG-5");
+                               $log.log(JSONLinks);
+                                $log.log(JSONTags);
                                 var link = svg.selectAll(".link")
-                                              .data($scope.JSONLinks)
+                                              .data(JSONLinks)
                                             .enter().append("line")
                                               .attr("class", "link")
                                               .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-                                var node = svg.selectAll(".node")
-                                              .data($scope.JSONTags)
+                                 $log.log("FDG-5"); 
+                               var node = svg.selectAll(".node")
+                                              .data(JSONTags)
                                             .enter().append("circle")
                                               .attr("class", "node")
-                                              .attr("r", 5)
-                                              .style("fill", function(d) { return color(d.Color); })
+                                              .attr("r", 20)
+                                              .style("fill", function(d) { return d.Color; })
                                               .call(force.drag);
                                 node.append("title")
-                                    .text(function(d) { return d.Tag; });
+                                    .text(function(d) { return d.Tag });
+                                                            
                                 force.on("tick", function() {
                                                     link.attr("x1", function(d) { return d.source.x; })
                                                         .attr("y1", function(d) { return d.source.y; })
@@ -202,231 +171,12 @@
                                     node.attr("cx", function(d) { return d.x; })
                                         .attr("cy", function(d) { return d.y; });
                                     });
-                            }
+                            $log.log("END-DFG"); 
+                           }
                     
                     }
                 }
 
         }]);
-    /*
-
-    app.directive('zoomableBubble', function(){
-      function link(scope, el, attr){
-        var margin = 20,
-            diameter = 960;
-
-        var color = d3.scale.linear()
-            .domain([-1, 5])
-            .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-            .interpolate(d3.interpolateHcl);
-
-        var pack = d3.layout.pack()
-            .padding(2)
-            .size([diameter - margin, diameter - margin])
-            .value(function(d) { return d.size; })
-
-        var svg = d3.select("k").append("svg")
-            .attr("width", diameter)
-            .attr("height", diameter)
-          .append("g")
-            .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
-        
-          d3.json("/../MarkIt/static/flare.json", function(error, root) {
-          if (error) return console.error(error);
-
-          var focus = root,
-              nodes = pack.nodes(root),
-              view;
-
-          var circle = svg.selectAll("circle")
-              .data(nodes)
-            .enter().append("circle")
-              .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
-              .style("fill", function(d) { return d.children ? color(d.depth) : null; })
-              .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
-
-          var text = svg.selectAll("text")
-              .data(nodes)
-            .enter().append("text")
-              .attr("class", "label")
-              .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
-              .style("display", function(d) { return d.parent === root ? null : "none"; })
-              .text(function(d) { return d.name; });
-
-          var node = svg.selectAll("circle,text");
-
-          d3.select("k")
-              .style("background", color(-1))
-              .on("click", function() { zoom(root); });
-
-          zoomTo([root.x, root.y, root.r * 2 + margin]);
-
-          function zoom(d) {
-            var focus0 = focus; focus = d;
-
-            var transition = d3.transition()
-                .duration(d3.event.altKey ? 7500 : 750)
-                .tween("zoom", function(d) {
-                  var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
-                  return function(t) { zoomTo(i(t)); };
-                });
-
-            transition.selectAll("text")
-              .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
-                .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
-                .each("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
-                .each("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
-          }
-
-          function zoomTo(v) {
-            var k = diameter / v[2]; view = v;
-            node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
-            circle.attr("r", function(d) { return d.r * k; });
-          }
-        });
-        d3.select(self.frameElement).style("height", diameter + "px");       
-      }
-      return {
-        link: link,
-        restrict: 'E',
-        scope: { data: '=' }
-      };
-    });*/
-   /* app.directive('d3Bubble',['d3Service',function(d3Service){
-            return{
-                restrict:'EA',
-            }
-            
-    
-        }]);
-    app.directive('d3Bars', ['$window', '$timeout', 'd3Service', 
-  function($window, $timeout, d3Service) {
-    return {
-      restrict: 'A',
-      scope: {
-        data: '=',
-        onClick: '&'
-      },
-      link: function(scope, ele, attrs) {
-        d3Service.d3().then(function(d3) {
-
-          var renderTimeout;
-          var margin = parseInt(attrs.margin) || 20,
-              barHeight = parseInt(attrs.barHeight) || 20,
-              barPadding = parseInt(attrs.barPadding) || 5;
-
-          var svg = d3.select(ele[0])
-            .append('svg')
-            .style('width', '100%');
-
-          $window.onresize = function() {
-            scope.$apply();
-          };
-            scope.data = [
-            {name: "Greg", score: 98},
-            {name: "Ari", score: 96},
-            {name: 'Q', score: 75},
-            {name: "Loser", score: 48}
-          ];
-
-          scope.$watch(function() {
-            return angular.element($window)[0].innerWidth;
-          }, function() {
-            scope.render(scope.data);
-          });
-
-          scope.$watch('data', function(newData) {
-            scope.render(newData);
-          }, true);
-
-          scope.render = function(data) {
-            svg.selectAll('*').remove();
-
-            if (!data) return;
-            if (renderTimeout) clearTimeout(renderTimeout);
-
-            renderTimeout = $timeout(function() {
-              var width = d3.select(ele[0]).node().offsetWidth - margin,
-                  height = scope.data.length * (barHeight + barPadding),
-                  color = d3.scale.category20(),
-                  xScale = d3.scale.linear()
-                    .domain([0, d3.max(data, function(d) {
-                      return d.score;
-                    })])
-                    .range([0, width]);
-
-              svg.attr('height', height);
-
-              svg.selectAll('rect')
-                .data(data)
-                .enter()
-                  .append('rect')
-                  .on('click', function(d,i) {
-                    return scope.onClick({item: d});
-                  })
-                  .attr('height', barHeight)
-                  .attr('width', 140)
-                  .attr('x', Math.round(margin/2))
-                  .attr('y', function(d,i) {
-                    return i * (barHeight + barPadding);
-                  })
-                  .attr('fill', function(d) {
-                    return color(d.score);
-                  })
-                  .on('click', function(d, i) {
-                    return scope.onClick({item: d});
-                  })
-                  .transition()
-                    .duration(1000)
-                    .attr('width', function(d) {
-                      return xScale(d.score);
-                    });
-              svg.selectAll('text')
-                .data(data)
-                .enter()
-                  .append('text')
-                  .attr('fill', '#fff')
-                  .attr('y', function(d,i) {
-                    return i * (barHeight + barPadding) + 15;
-                  })
-                  .attr('x', 15)
-                  .text(function(d) {
-                    return d.name + " (" + d.score + ")";
-                  });
-            }, 100);
-          };
-        });
-      }}
-}])*/
- /*    app.directive('donutChart', function(){
-      function link(scope, el, attr){
-        var color = d3.scale.category10();
-        var data = scope.data;
-        var width = 300;
-        var height = 300;
-        var min = Math.min(width, height);
-        var svg = d3.select(el[0]).append('svg');
-        var pie = d3.layout.pie().sort(null);
-        var arc = d3.svg.arc()
-          .outerRadius(min / 2 * 0.9)
-          .innerRadius(min / 2 * 0.5);
-    
-        svg.attr({width: width, height: height});
-        var g = svg.append('g')
-          // center the donut chart
-          .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-        
-        // add the <path>s for each arc slice
-        g.selectAll('path').data(pie(data))
-          .enter().append('path')
-            .style('stroke', 'white')
-            .attr('d', arc)
-            .attr('fill', function(d, i){ return color(i) });
-      }
-      return {
-        link: link,
-        restrict: 'E',
-        scope: { data: '=' }
-      };
-    });*/   
+  
 }());
