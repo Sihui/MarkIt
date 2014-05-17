5773ec9657c3c009f29c86d0becc0eaee2dcb9ef
Sihui Huang <sihuih@gmail.com>
2014-05-17 11:58:20 -0500

    IN directivedForceGraph directive: almost done with function

--- static/ng-app.js
+++ static/ng-app.js
@@ -43,7 +43,7 @@
                 }
             };
         });
-    app.directive('directedforcedGraph',['$filter','$log',function($filter,$log){
+    app.directive('forcedirectedGraph',['$filter','$log',function($filter,$log){
              /*function createLink (s,t,v){
                   return {source: s,
                         target: t,
@@ -90,15 +90,18 @@
             return {
                 restrict: 'E',
                 scope: { tags: '='},
-                template:'<p>{{getLinks(tags)}}</p>',
-                link: function ( $scope, $element){
+                template:'<p ng-style="ForceDirectedGraph()">{{}}</p>',
+                link: function ( $scope, $element,attr){
+                     
                             function createLink (s,t,v){
+                                
                                                     return {source: s,
                                                             target: t,
                                                              value:v};
                                                 };
                             
                             $scope.getLinks = function(tags){
+                                
                                         $scope.links = [];
                                         angular.forEach(tags, function(tag){
                                             if(tag.Friends!=0){
@@ -146,49 +149,69 @@
                                                 }                  
                                             }
                                         });
-                                $log.log(angular.toJson($scope.links));
-                                        return angular.toJson($scope.links);                  
+                                        $scope.JSONLinks = angular.toJson($scope.links); 
+                                        $scope.JSONTags = angular.toJson(tags); 
+                                 //$log.log(angular.toJson($scope.links));
+                                         //$log.log("LOOK");
+                                        // $log.log($scope.JSONTags);
+                                return $scope.JSONLinks
+                                        
+                            
+                            }
+                            
+                            //
+                           function ForceDirectedGraph(){
+                                $log.log("LOOK");
+                                $scope.getLinks(tags);
+                                var width = 960,
+                                    height = 500;
+
+                                var color = d3.scale.category20();
+
+                                var force = d3.layout.force()
+                                    .charge(-120)
+                                    .linkDistance(30)
+                                    .size([width, height]);
+
+                                var svg = d3.select("body").append("svg")
+                                    .attr("width", width)
+                                    .attr("height", height);
+                                force
+                                      .nodes($scope.JSONTags)
+                                      .links($scope.JSONLinks)
+                                      .start();
+                                var link = svg.selectAll(".link")
+                                              .data($scope.JSONLinks)
+                                            .enter().append("line")
+                                              .attr("class", "link")
+                                              .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+                                var node = svg.selectAll(".node")
+                                              .data($scope.JSONTags)
+                                            .enter().append("circle")
+                                              .attr("class", "node")
+                                              .attr("r", 5)
+                                              .style("fill", function(d) { return color(d.Color); })
+                                              .call(force.drag);
+                                node.append("title")
+                                    .text(function(d) { return d.Tag; });
+                                force.on("tick", function() {
+                                                    link.attr("x1", function(d) { return d.source.x; })
+                                                        .attr("y1", function(d) { return d.source.y; })
+                                                        .attr("x2", function(d) { return d.target.x; })
+                                                        .attr("y2", function(d) { return d.target.y; });
+                                    node.attr("cx", function(d) { return d.x; })
+                                        .attr("cy", function(d) { return d.y; });
+                                    });
                             }
                     
                     }
                 }
 
         }]);
-    
-    app.directive('donutChart', function(){
-      function link(scope, el, attr){
-        var color = d3.scale.category10();
-        var data = scope.data;
-        var width = 300;
-        var height = 300;
-        var min = Math.min(width, height);
-        var svg = d3.select(el[0]).append('svg');
-        var pie = d3.layout.pie().sort(null);
-        var arc = d3.svg.arc()
-          .outerRadius(min / 2 * 0.9)
-          .innerRadius(min / 2 * 0.5);
-    
-        svg.attr({width: width, height: height});
-        var g = svg.append('g')
-          // center the donut chart
-          .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-        
-        // add the <path>s for each arc slice
-        g.selectAll('path').data(pie(data))
-          .enter().append('path')
-            .style('stroke', 'white')
-            .attr('d', arc)
-            .attr('fill', function(d, i){ return color(i) });
-      }
-      return {
-        link: link,
-        restrict: 'E',
-        scope: { data: '=' }
-      };
-    });
+    /*
+
     app.directive('zoomableBubble', function(){
       function link(scope, el, attr){
-        //var data = scope.data;
         var margin = 20,
             diameter = 960;
 
@@ -268,7 +291,7 @@
         restrict: 'E',
         scope: { data: '=' }
       };
-    });
+    });*/
    /* app.directive('d3Bubble',['d3Service',function(d3Service){
             return{
                 restrict:'EA',
@@ -375,5 +398,35 @@
         });
       }}
 }])*/
+ /*    app.directive('donutChart', function(){
+      function link(scope, el, attr){
+        var color = d3.scale.category10();
+        var data = scope.data;
+        var width = 300;
+        var height = 300;
+        var min = Math.min(width, height);
+        var svg = d3.select(el[0]).append('svg');
+        var pie = d3.layout.pie().sort(null);
+        var arc = d3.svg.arc()
+          .outerRadius(min / 2 * 0.9)
+          .innerRadius(min / 2 * 0.5);
     
+        svg.attr({width: width, height: height});
+        var g = svg.append('g')
+          // center the donut chart
+          .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+        
+        // add the <path>s for each arc slice
+        g.selectAll('path').data(pie(data))
+          .enter().append('path')
+            .style('stroke', 'white')
+            .attr('d', arc)
+            .attr('fill', function(d, i){ return color(i) });
+      }
+      return {
+        link: link,
+        restrict: 'E',
+        scope: { data: '=' }
+      };
+    });*/   
 }());
