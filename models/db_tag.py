2157b220912332ad18c4f75ee00ddac1c2e524fc
Sihui Huang <sihuih@gmail.com>
2014-05-17 07:54:09 -0500

    added friendslist in tag.db

--- models/db_tag.py
+++ models/db_tag.py
@@ -4,7 +4,7 @@ db.define_table('tag',
                 Field('Owner',db.auth_user,default=(auth.user_id if auth.is_logged_in() else long('537372c49256a31f4fdf6be5',16))),
                 Field('URLs','list:string',default=[]),
                 Field('Size','integer',default=1),
-                #Field('Selected','boolean',default=False),
+                Field('Friends','list:string'),
                 format='%(Tag)s')
 db.tag.Owner.readable = db.tag.Owner.writable = False
 db.tag.Tag.requires=[IS_NOT_IN_DB(db, 'tag.Tag',error_message='Dupilcated Tag'),
