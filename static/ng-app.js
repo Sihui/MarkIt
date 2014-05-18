7fd990479530c2bc6cff1aae646d84776975b6a9
Sihui Huang <sihuih@gmail.com>
2014-05-17 23:43:48 -0500

    beautified fdg

--- static/ng-app.js
+++ static/ng-app.js
@@ -35,7 +35,7 @@
             return {
                 restrict: 'E',
                 scope: { url: '='},
-                template:'<div class="card">'+'<div class="card-heading simple">'+'<a href="//{{url.URL}}" target="_blank">{{url.Title}}</a></div>'+'<div class="card-body"><p>{{url.Note}}</p></div>'+'<div class="card-actions">'+'<tagSection ng-repeat="(i,tag) in url.Tags"  class="tag"  ng-style="set_color(url.Colors[i])">{{tag}}</tagSection>'+'</div></div>',
+                template:'<div class="card">'+'<div class="card-heading simple">'+'<a href="//{{url.URL}}" target="_blank">{{url.Title}}</a></div>'+'<div class="card-body"><p>{{url.Note}}</p></div>'+'<div class="card-actions">'+'<tagSection ng-repeat="(i,tag) in url.Tags"  class="tag"  ng-style="set_color(url.Colors[i])"><span>{{tag}}</span></tagSection>'+'</div></div>',
                 link: function ( $scope, $element ) {
                      $scope.set_color = function (c) {
                             return { color: c}
@@ -55,13 +55,9 @@
                                                             value:v};
                                                 };
                             $scope.fdg = function(){
-                                //$log.log("INIT-DATA");
-                                //$log.log(tags);
-                                //$log.log("END-INIT-DATA");
                                 $log.log("in function");
                                 $scope.getLinks();
-                                ForceDirectedGraph($scope.tags,$scope.links,$scope.tags);
-                                //return 1;
+                                ForceDirectedGraph($scope.tags,$scope.links);
                             }
                             $scope.getLinks = function(){
                                         $scope.links = [];
@@ -108,101 +104,59 @@
                                                                                                   
                             }
                            function ForceDirectedGraph(tags,links){
-                                $scope.getLinks(tags);
-                                //$log.log("FDG-1");
-                                var width = 900,
-                                    height = 400;
-                               //$log.log("FDG-2");
-                                var force = d3.layout.force()
-                                    .charge(-120)
-                                    .linkDistance(60)
-                                    .size([width, height])
-                                    .nodes(tags)
-                                    .links(links)
-                                    .start();
-                               var w = window,
-                                    d = document,
-                                    e = d.documentElement,
-                                    g = d.getElementsByTagName('body')[0],
-                                    x = w.innerWidth || e.clientWidth || g.clientWidth,
-                                    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-                               /* for some reasone this directive load multipule times 
-                                  (twice for urlFactory, twice for tagFactory, and twice for the page/driective itself)
-                                  so I have to remove previouse svg before appending a new one*/
-                               d3.select("svg") .remove();
-                                var svg = d3.select($element[0]).append("svg")
-                                            .attr("width", x)
-                                            .attr("height", y)
-                                var link = svg.selectAll(".link")
+                                d3.select("svg") .remove();
+                                var svg = d3.select($element[0]).append("svg");
+                                var force = d3.layout.force();
+                                          force
+                                              .nodes(tags)
+                                              .links(links)
+                                          .charge(-120)
+                                          .linkDistance(60)
+                                          
+                                              .on("tick", tick)
+                                              .start();
+
+                                          var link = svg.selectAll(".link")
                                               .data(links)
                                             .enter().append("line")
-                                              .attr("class", "link")
-                                              .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-                              
+                                              .attr("class", "link");
                                var node = svg.selectAll(".node")
-                                              .data(tags)
-                                            .enter().append("circle")
-                                              .attr("class", "node")
-                                              .attr("r", 20)
-                                              .style("fill", function(d) { return d.Color; })
-                                              .call(force.drag);
-                                node.append("title")
-                                    .text(function(d) { return d.Tag });/*
-                                 var node = svg.append("g").selectAll(".node")
-                                                  .data(tags)
-                                                .enter().append("circle")
-                                                  .attr("r", 6)
-                                                  .call(force.drag);
-                               var text = svg.append("g").selectAll("g")
-                                                  .data(nodes)
-                                                .enter().append("g");
+                                                .data(tags)
+                                              .enter().append("g")
+                                                .attr("class", "node")
+                                                .call(force.drag);
 
-                                              text.append("text")
-                                                  .attr("x", 8)
-                                                  .attr("y", ".31em")
-                                                  .attr("class", "shadow")
-                                                  .text(function(d) { return d.Tag; });
+                                            node.append("circle")
+                                            .style("fill", function(d) { return d.Color; })
+                                                .attr("r", 8);
 
-                                              text.append("text")
-                                                  .attr("x", 8)
-                                                  .attr("y", ".31em")
-                                                  .text(function(d) { return d.Tag; });*/
-                               /* var g = svg.selectAll("g.node")
-                                      .data(nodes)
-                                    .enter().append("svg:g")
-                                      .attr("class", "node")
-                                      .attr("transform", function(d) { return "translate(" + d.x + ","
-                                + d.y + ")"; })
-                                      .call(force.drag);
+                                            node.append("text")
+                                                .attr("x", 12)
+                                                .attr("dy", ".35em")
+                                                .text(function(d) { return d.Tag; });
 
-                                  g.append("svg:circle")
-                                      .attr("r", 5);
+                                          resize();
+                                          d3.select(window).on("resize", resize);
 
-                                  g.append("svg:text")
-                                      .attr("x", 10)
-                                      .attr("dy", ".31em")
-                                      .text(function(d) { return d.name; });*/
-                                                            
-                                force.on("tick", function() {
-                                                    link.attr("x1", function(d) { return d.source.x; })
-                                                        .attr("y1", function(d) { return d.source.y; })
-                                                        .attr("x2", function(d) { return d.target.x; })
-                                                        .attr("y2", function(d) { return d.target.y; });
-                                    node.attr("cx", function(d) { return d.x; })
-                                        .attr("cy", function(d) { return d.y; });
-                                    });
-                               function updateWindow(){
-    x = w.innerWidth || e.clientWidth || g.clientWidth;
-    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-    
-    svg.attr("width", x).attr("height", y);
-}
-window.onresize = updateWindow;
-                           // $log.log("END-DFG"); 
-                           }
-                    
-                    }
-                }
+                                          function tick() {
+                                            link.attr("x1", function(d) { return d.source.x; })
+                                                .attr("y1", function(d) { return d.source.y; })
+                                                .attr("x2", function(d) { return d.target.x; })
+                                                .attr("y2", function(d) { return d.target.y; });
+
+                                            node.attr("cx", function(d) { return d.x; })
+                                              .attr("cy", function(d) { return d.y; });
+                                              node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+                                          }
+
+                                          function resize() {
+                                            width = window.innerWidth, height = window.innerHeight;
+                                            svg.attr("width", width).attr("height", height/3);
+                                            force.size([width, height/3]).resume();
+                                          }
+                                }                
+                            }
+                        }
 
         }]);
   
