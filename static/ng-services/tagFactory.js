144d9d5bf330d40e85795d6ab4597be674124539
Sihui Huang <sihuih@gmail.com>
2014-05-19 23:46:05 -0500

    readme

--- static/ng-services/tagFactory.js
+++ static/ng-services/tagFactory.js
@@ -1,12 +1,12 @@
 (function(){
     
     var tagFactory = function($http){
-        $http.defaults.headers.common['Authorization'] = 'api-key ';
+        $http.defaults.headers.common['Authorization'] = 'api-key BcnBRbwH9q3O9Yyut3e4Ee8iB06aYQ7DvngI02';
         $http.defaults.headers.common['Content-Type'] = 'application/json';
         var factory = {};
         
         factory.getTags = function(){
-            return $http.get('https://beta-api.mongohq.com/mongo//MarkIt/collections/tag/documents?limit=100');
+            return $http.get('https://beta-api.mongohq.com/mongo/530ce7bd23ff0a6078000074/MarkIt/collections/tag/documents?limit=100');
         }
         
         return factory;
