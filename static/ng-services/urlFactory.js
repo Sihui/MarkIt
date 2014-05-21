5d1c67f626ba93c599f16cf1fb706f5a33b17893
Sihui Huang <sihuih@gmail.com>
2014-05-20 22:06:58 -0500

    updated readme

--- static/ng-services/urlFactory.js
+++ static/ng-services/urlFactory.js
@@ -1,12 +1,12 @@
 (function(){
     
     var urlFactory = function($http){
-        $http.defaults.headers.common['Authorization'] = 'api-key BcnBRbwH9q3O9Yyut3e4Ee8iB06aYQ7DvngI02';
+        $http.defaults.headers.common['Authorization'] = 'api-key ';
         $http.defaults.headers.common['Content-Type'] = 'application/json';
         var factory = {};
         
         factory.getUrls = function(){
-            return $http.get('https://beta-api.mongohq.com/mongo/530ce7bd23ff0a6078000074/MarkIt/collections/url/documents?limit=100');
+            return $http.get('https://beta-api.mongohq.com/mongo//MarkIt/collections/url/documents?limit=100');
         }
         
         return factory;
